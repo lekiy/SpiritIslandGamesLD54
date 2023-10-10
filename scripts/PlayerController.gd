80b063344f2f335e85ada45505f37b6ca3109c86
Kevin Annen <lekiyminin@gmail.com>
2023-10-10 09:20:07 -0500

    Working on multiselect

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -2,39 +2,52 @@ extends Camera2D
 
 @export var scroll_speed = 100
 
-var select_start : Vector2
-var select_end : Vector2
-var is_selecting := false
+var drag_start : Vector2
+var dragging := false
+var select_rect = RectangleShape2D.new()
+var selected = []
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var scroll_direction = Vector2(Input.get_axis("move_cam_left", "move_cam_right"), Input.get_axis("move_cam_up", "move_cam_down")).normalized()
 
 	position += scroll_direction*scroll_speed*delta;
 	queue_redraw()
 
+
 func _input(event):
-	if(event is InputEventMouseButton):
-		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT and !is_selecting:
-			select_start = event.position - Vector2(160, 90)
-			select_end = select_start
-			is_selecting = true
-		elif event.is_released() and event.button_index == MOUSE_BUTTON_LEFT:
-			is_selecting = false
-			select_end = event.position - Vector2(160, 90)
-	if event is InputEventMouseMotion and is_selecting:
-		select_end = event.position - Vector2(160, 90)
+	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT):
+		if(event.is_pressed()):
+			if selected.size() == 0:
+				dragging = true
+				drag_start = event.position- Vector2(160, 90)
+		elif dragging:
+			dragging = false
+	if event is InputEventMouseMotion and dragging:
+		pass # update()
+# 		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT and !is_selecting:
+# 			select_start = event.position - Vector2(160, 90)
+# 			select_end = select_start
+# 			is_selecting = true
+# 		elif event.is_released() and event.button_index == MOUSE_BUTTON_LEFT:
+# 			is_selecting = false
+# 			select_end = event.position - Vector2(160, 90)
+# 			# select_shape.shape = RectangleShape2D.new()
+# 			select_shape.shape.size = select_end-select_start
+# 			select_shape.position = select_start
+# 			var selected_nodes = select_area.get_overlapping_areas()
+# 			for area in selected_nodes:
+# 				if(area is SelectionComponent):
+# 					area.selected = true
+# 	if event is InputEventMouseMotion and is_selecting:
+# 		select_end = event.position - Vector2(160, 90)
+		
 
 	
 
 func _draw():
-	# if(is_selecting):
-		var rect = Rect2(select_start, select_end-select_start)
+	if(dragging):
+		var rect = Rect2(drag_start, get_global_mouse_position()-drag_start-position)
+		print(rect)
 		draw_rect(rect, Color.ALICE_BLUE, false)
 
 	
