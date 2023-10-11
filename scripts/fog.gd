6055d0e4277ee5bf35fefc2a33efd434c1918b63
Kevin Annen <lekiyminin@gmail.com>
2023-10-11 00:51:48 -0500

    fix crash

--- scripts/fog.gd
+++ scripts/fog.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 
-var shader_clear_texture : Texture2D = material.get_shader_param("clear")
+# var shader_clear_texture : Texture2D = material.get_shader_param("clear")
 
 func _ready():
 	pass
