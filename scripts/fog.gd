54204626f79ae72a317f44d76f4b6981be04747f
Kevin Annen <lekiyminin@gmail.com>
2023-10-13 00:58:40 -0500

    Added basic mask to fog shader

--- scripts/fog.gd
+++ scripts/fog.gd
@@ -5,13 +5,18 @@ extends Sprite2D
 
 func _ready():
 	var image = Image.create(60*20, 60*20, false, Image.Format.FORMAT_RGBA8)
-	image.fill_rect(Rect2i(Vector2i(0, 0), Vector2i(100, 100)), Color.WHITE)
+	image.fill_rect(Rect2i(Vector2i(0, 0), Vector2i(60*20, 60*20)), Color.WHITE)
 
-	var circle_center = Vector2(50, 50);
-	var circle_radius = 10*20;
+	var circle_center = Vector2(image.get_width()*0.5, image.get_height()*0.5);
+	var circle_radius = 3*20;
 	for x in image.get_width():
 		for y in image.get_height():
-			if(circle_center.distance_to(Vector2(x, y)) <= circle_radius):
-				image.set_pixel(x, y, Color(0, 0, 0, 0))
-	# material.set("shader_parameter/mask", image)
+			var dist = circle_center.distance_to(Vector2(x, y))
+			var fade_scale = 5
+			if(dist <= circle_radius-20):
+				image.set_pixel(x, y, Color(1, 1, 1, 0))
+			else:
+				image.set_pixel(x, y, Color(1, 1, 1, (dist-(circle_radius-fade_scale))/(fade_scale)))
+	material.set("shader_parameter/mask", ImageTexture.create_from_image(image))
+	print(material.get("shader_parameter/mask"))
 	# self.texture = ImageTexture.create_from_image(image)
